b9300fb55591c91b1d025b1cae1c1fc7eb60bcdb
Mateo-Sanchez14 <mateo-sanchez@hotmail.com>
2024-06-12 15:50:34 -0300

    update ip

--- src/helpers/apiCalls.ts
+++ src/helpers/apiCalls.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { PositionInterface } from "./interfaces";
 import { parsePositionRequest } from "./apiParsers";
 
-const API_BASE_URL = "http://192.168.0.102:8000";
+// const API_BASE_URL = "http://192.168.0.102:8000";
 
 const POSITION_ENDPOINT = "/api/v1/posicion";
 
-axios.defaults.baseURL = API_BASE_URL;
+// axios.defaults.baseURL = API_BASE_URL;
 // Disable axios protection policies
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
